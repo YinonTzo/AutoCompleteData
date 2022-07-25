a7205123181acbf3c391ec8209a1a668caefd0d4
eindor <eindor13@gmail.com>
2022-07-25 20:49:55 +0300

    finished offline word dictionary build

--- main.py
+++ main.py
@@ -2,18 +2,14 @@ import os
 
 if __name__ == '__main__':
     wordDict = {}
+
     for root, dirs, files in os.walk('resources'):
-        for file in files:
-            print(os.path.abspath(os.path.join(root, file)))
-
-
-    """   f = open(file, "r")
-            for line in f:
-                for word in line:
-                    if word not in wordDict.keys():
-                        wordDict[word] = []
-                    wordDict[word].append(line)
+        for file in files:  #get each file within the directory and subdirectories
+            path = (os.path.abspath(os.path.join(root, file)))  #get the full path of each file
+            with open(path, encoding="utf8") as f:  #open the file
+                for line in f:
+                    for word in line.split(" "):    #every word seperated by space
+                        if word not in wordDict.keys():
+                            wordDict[word] = []
+                        wordDict[word].append(line) #add the line to the key word
     print(wordDict)
-    """
-
-
