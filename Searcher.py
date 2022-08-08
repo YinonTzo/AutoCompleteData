3311308e29369da8796d5fdc27895930013fd698
Yinon Tzomi <92912589+YinonTzo@users.noreply.github.com>
2022-08-08 16:17:58 +0300

    fix word works

--- Searcher.py
+++ Searcher.py
@@ -60,25 +60,24 @@ class Searcher:
         prefix = split_user_input[:misspelled_word_place]
         suffix = split_user_input[misspelled_word_place + 1:]
 
+        fix_sentences = list()
         for line in intersection_of_lines:
             split_sentence = self.data.get_data_sentence_to_file(line).get_sentence().split(" ")
 
             first_match_index = split_sentence.index(prefix[0])
             real_word_index = first_match_index + len(prefix)
 
-            if self.check_pattern_match(first_match_index, split_sentence, prefix):
-                print("prefix match")
-            else:
-                print("prefix doesn't match")
+            if self.check_pattern_match(first_match_index, split_sentence, prefix) and \
+                    self.check_pattern_match(real_word_index + 1, split_sentence, suffix):
+                if self.fix_word(misspelled_word, split_sentence[real_word_index]):
+                    fix_sentences.append(AutoCompleteData(
+                        self.data.get_data_sentence_to_file(line).get_sentence(),
+                        self.data.get_data_sentence_to_file(line).get_file_name(),
+                        0, 0))
+                elif len(suffix) == 0:  # it means that all the words are ok but the last.
+                    return self.find_complete_sentence(user_input, intersection_of_lines)[:5]
 
-            if self.check_pattern_match(real_word_index + 1, split_sentence, suffix):
-                print("suffix match")
-            else:
-                print("suffix doesn't match")
-
-            self.try_to_fix_word(misspelled_word, split_sentence[real_word_index])
-
-        return []
+        return fix_sentences[:5]
 
     def check_pattern_match(self, index, complete_sentence, partial_input):
         if len(partial_input) > len(complete_sentence[index:]):
@@ -91,81 +90,32 @@ class Searcher:
                 return False
         return True
 
-    def try_to_fix_word(self, misspelled_word, real_word):
-        pass
+    def fix_word(self, misspelled_word, real_word):
+        if self.fix_by_delete_letter(misspelled_word, real_word):
+            print("delete word")
+            return True
+        if self.fix_by_add_letter(misspelled_word, real_word):
+            print("add word")
+            return True
+        if self.fix_by_change_letter(misspelled_word, real_word):
+            print("change word")
+            return True
 
+    def fix_by_delete_letter(self, misspelled_word, real_word):
+        for i in range(0, len(misspelled_word)):
+            if misspelled_word[:i] + misspelled_word[i + 1:] == real_word:
+                return True
+        return False
 
-#     for id in numbers_of_lines:
-#     #for id in make_intersection(prefix_list):
-#         sentence = data.get_data_sentence_to_file(id)
-#         if sentence.get_sentence().lower().find(prefix) != -1:
-#             results.append(AutoCompleteData(id, prefix, sentence.get_sentence(), sentence.get_line(), 0, sentence.get_file_name()))
+    def fix_by_add_letter(self, misspelled_word, real_word):
+        return self.fix_by_delete_letter(real_word, misspelled_word)
 
-# def get_score(sentence: str, list: list[bool, ])
-
-#
-# def get_best_k_completions(input : str) -> list[str]:
-#     """
-#     :param input:
-#     :return:
-#     """
-#     perfect_list = get_perfect_match(input)
-#     perfect_list = list()
-#     #perf_list = list()
-#     max_score = 0
-#     #if len(perfect_list) != 0:
-#     input_by_words = input.split(" ")
-#     for i in range(len(input_by_words)):
-#         prefix = input_by_words[:i]
-#         suffix = input_by_words[i+1:]
-#         full_prefix_string = ' '.join(map(str, prefix))
-#         full_suffix_string = ' '.join(map(str, suffix))
-#         prefix_list = get_perfect_match(full_prefix_string)
-#         suffix_list = get_perfect_match(full_suffix_string)
-#         if len(prefix_list) == 0 or len(suffix_list) == 0:
-#             continue
-#
-#         prefix_set = get_id_set(prefix_list)
-#         suffix_set = get_id_set(suffix_list)
-#         possible_match_list = prefix_set.intersection(suffix_set)
-#         for id in possible_match_list:
-#             sentence = data.get_data_sentence_to_file(id).get_sentence()
-#             sentence = sentence.lower()
-#             prefix_index = sentence.find(' ' + full_prefix_string.lower() + ' ')
-#             suffix_index = sentence.find(' ' + full_suffix_string.lower() + ' ')
-#             if prefix_index < suffix_index:
-#                 fix_parameters = fix_words(input_by_words[i], sentence[prefix_index + len(full_prefix_string) + 2: suffix_index])
-#                 if fix_parameters[0] is True:
-#                     perfect_list.append(sentence)
-#                 if len(perfect_list) == 5:
-#                     return perfect_list
-#     return perfect_list
-
-
-def get_id_set(li: list[AutoCompleteData]) -> set[int]:
-    sentence_id_set = set()
-    for item in li:
-        sentence_id_set.add(item.get_id())
-    return sentence_id_set
-
-
-def print_user_input() -> None:
-    user_search = ""
-    while user_search != "#":
-        user_search = input('Enter your text:')
-        counter = 0
-        list = get_perfect_match(user_search)
-        if len(list) != 0:
-            for result in list:
-                counter += 1
-                if counter == 6:
-                    break
-                print(str(counter) + ". " + result.get_str())
-        else:
-            for result in get_best_k_completions(user_search):
-                counter += 1
-                print(str(counter) + ". " + result)
-        print("\n")
+    def fix_by_change_letter(self, misspelled_word, real_word):
+        for i in range(0, len(misspelled_word)):
+            if misspelled_word[:i] == real_word[:i] and \
+                    misspelled_word[i + 1:] == real_word[i + 1:]:
+                return True
+        return False
 
 
 def fix_by_delete(target_word: str, wanted_word: str) -> str:
@@ -237,3 +187,77 @@ def fix_words(target_word: str, wanted_word: str) -> list:
     if fix_index != -1:
         return [True, fix_index, 3]
     return [False, 0, 0]
+
+#     for id in numbers_of_lines:
+#     #for id in make_intersection(prefix_list):
+#         sentence = data.get_data_sentence_to_file(id)
+#         if sentence.get_sentence().lower().find(prefix) != -1:
+#             results.append(AutoCompleteData(id, prefix, sentence.get_sentence(), sentence.get_line(), 0, sentence.get_file_name()))
+
+# def get_score(sentence: str, list: list[bool, ])
+
+#
+# def get_best_k_completions(input : str) -> list[str]:
+#     """
+#     :param input:
+#     :return:
+#     """
+#     perfect_list = get_perfect_match(input)
+#     perfect_list = list()
+#     #perf_list = list()
+#     max_score = 0
+#     #if len(perfect_list) != 0:
+#     input_by_words = input.split(" ")
+#     for i in range(len(input_by_words)):
+#         prefix = input_by_words[:i]
+#         suffix = input_by_words[i+1:]
+#         full_prefix_string = ' '.join(map(str, prefix))
+#         full_suffix_string = ' '.join(map(str, suffix))
+#         prefix_list = get_perfect_match(full_prefix_string)
+#         suffix_list = get_perfect_match(full_suffix_string)
+#         if len(prefix_list) == 0 or len(suffix_list) == 0:
+#             continue
+#
+#         prefix_set = get_id_set(prefix_list)
+#         suffix_set = get_id_set(suffix_list)
+#         possible_match_list = prefix_set.intersection(suffix_set)
+#         for id in possible_match_list:
+#             sentence = data.get_data_sentence_to_file(id).get_sentence()
+#             sentence = sentence.lower()
+#             prefix_index = sentence.find(' ' + full_prefix_string.lower() + ' ')
+#             suffix_index = sentence.find(' ' + full_suffix_string.lower() + ' ')
+#             if prefix_index < suffix_index:
+#                 fix_parameters = fix_words(input_by_words[i], sentence[prefix_index + len(full_prefix_string) + 2: suffix_index])
+#                 if fix_parameters[0] is True:
+#                     perfect_list.append(sentence)
+#                 if len(perfect_list) == 5:
+#                     return perfect_list
+#     return perfect_list
+
+
+#
+# def get_id_set(li: list[AutoCompleteData]) -> set[int]:
+#     sentence_id_set = set()
+#     for item in li:
+#         sentence_id_set.add(item.get_id())
+#     return sentence_id_set
+#
+#
+# def print_user_input() -> None:
+#     user_search = ""
+#     while user_search != "#":
+#         user_search = input('Enter your text:')
+#         counter = 0
+#         list = get_perfect_match(user_search)
+#         if len(list) != 0:
+#             for result in list:
+#                 counter += 1
+#                 if counter == 6:
+#                     break
+#                 print(str(counter) + ". " + result.get_str())
+#         else:
+#             for result in get_best_k_completions(user_search):
+#                 counter += 1
+#                 print(str(counter) + ". " + result)
+#         print("\n")
+#
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import Searcher
 def main():
     searcher = Searcher.Searcher()
 
-    prefix = "line i"
+    prefix = "first lime"
     for i, completion in enumerate(searcher.get_best_k_completions(prefix)):
         print(str(i) + '. ' + str(completion))
 
