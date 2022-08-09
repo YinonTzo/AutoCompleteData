d1bb863c7cfe1934d8bb824d316b31cba5473ce9
Yinon Tzomi <92912589+YinonTzo@users.noreply.github.com>
2022-08-09 13:14:05 +0300

    Added documentation to AutoComplete

--- AutoCompleteData.py
+++ AutoCompleteData.py
@@ -3,15 +3,25 @@ from dataclasses import dataclass
 
 @dataclass
 class AutoCompleteData:
+    """A AutoCompleteData class.
 
-    def __init__(self, completed_sentence, source_text, offset, score):
-        self.completed_sentence = completed_sentence
+        Args:
+            complete_sentence (str): The completed_sentence.
+            source_text (str): Where the sentence comes from.
+            offset: Where the user input starts relative to the complete sentence.
+            score: The score of this sentence.
+
+        Attributes:
+
+        """
+    def __init__(self, complete_sentence, source_text, offset, score):
+        self.complete_sentence = complete_sentence
         self.source_text = source_text
         self.offset = offset
         self.score = score
 
     def __str__(self):
-        return self.completed_sentence + " (" + self.source_text + " " + str(self.offset) + ")"
+        return self.complete_sentence + " (" + self.source_text + " " + str(self.offset) + ")"
 
     def __repr__(self):
-        return self.completed_sentence + " (" + self.source_text + " " + str(self.offset) + ")"
+        return self.complete_sentence + " (" + self.source_text + " " + str(self.offset) + ")"
