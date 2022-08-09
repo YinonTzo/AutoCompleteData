81ce9f461d6eab8894d56bce24b1e8f8c0baa6a0
Yinon Tzomi <92912589+YinonTzo@users.noreply.github.com>
2022-08-09 13:27:00 +0300

    Added documentation to AutoComplete

--- AutoCompleteData.py
+++ AutoCompleteData.py
@@ -8,13 +8,13 @@ class AutoCompleteData:
         Args:
             complete_sentence (str): The completed_sentence.
             source_text (str): Where the sentence comes from.
-            offset: Where the user input starts relative to the complete sentence.
-            score: The score of this sentence.
+            offset (int): Where the user input starts relative to the complete sentence.
+            score (int): The score of this sentence.
 
         Attributes:
 
         """
-    def __init__(self, complete_sentence, source_text, offset, score):
+    def __init__(self, complete_sentence: str, source_text: str, offset: int, score: int):
         self.complete_sentence = complete_sentence
         self.source_text = source_text
         self.offset = offset
